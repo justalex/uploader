9d7671535462841bf90ce5735b13facf665f7011
Alex <just.alex@tut.by>
2010-12-09 22:21:29 +0200

    add functions

--- func.php
+++ func.php
@@ -1,71 +1,86 @@
-﻿<?php
+<?php
 
 function checkAuth( $mode ) {
     global $myDB, $myLogin, $myID;
-	if( $mode == 'check' ) {
-	    if( !$_COOKIE['login'] ) return 0;
-		
-		$cookie = explode( '###', base64_decode( $_COOKIE['login'] ) );
-		if( $row = $myDB->query( "SELECT id FROM users WHERE mail='".$myDB->secSQL( $cookie[0] )."' AND pass='".$myDB->secSQL( $cookie[1] )."'" ) ) {
-		    //return $myDB->secSQL( $cookie[0] );
-			$myLogin = $myDB->secSQL( $cookie[0] );
-			$myID    = $row[0]['id'];
-			return 1;
-		}
-		return 0;
-	}
-	if( $mode == 'login' ) {
-	    if( $row = $myDB->query( "SELECT pass FROM users WHERE mail='".$myDB->secSQL( $_POST['mail'] )."' AND pass='".md5(md5(md5($myDB->secSQL($_POST['pass']))))."'" ) AND $row != 1 ) {
-		    $cookie = base64_encode( $myDB->secSQL( $_POST['mail'] ).'###'.md5(md5(md5($myDB->secSQL($_POST['pass'])))) );
-			//print_r($row); exit;
-			setcookie( 'login', $cookie, time()+60*60*24*30 );
-		    header( 'Location: '.$_SERVER['PHP_SELF'] );
-		    return 1;
-		}
-		else return 0;
-	}
+    if( $mode == 'check' ) {
+        if( !$_COOKIE['login'] ) return 0;
+        
+        $cookie = explode( '###', base64_decode( $_COOKIE['login'] ) );
+        if( $row = $myDB->query( "SELECT id FROM users WHERE mail='".$myDB->secSQL( $cookie[0] )."' AND pass='".$myDB->secSQL( $cookie[1] )."'" ) ) {
+            //return $myDB->secSQL( $cookie[0] );
+            $myLogin = $myDB->secSQL( $cookie[0] );
+            $myID    = $row[0]['id'];
+            return 1;
+        }
+        return 0;
+    }
+    if( $mode == 'login' ) {
+        if( $row = $myDB->query( "SELECT pass FROM users WHERE mail='".$myDB->secSQL( $_POST['mail'] )."' AND pass='".md5(md5(md5($myDB->secSQL($_POST['pass']))))."'" ) AND $row != 1 ) {
+            $cookie = base64_encode( $myDB->secSQL( $_POST['mail'] ).'###'.md5(md5(md5($myDB->secSQL($_POST['pass'])))) );
+            //print_r($row); exit;
+            setcookie( 'login', $cookie, time()+60*60*24*30 );
+            header( 'Location: '.$_SERVER['PHP_SELF'] );
+            return 1;
+        }
+        else return 0;
+    }
 }
 
 function showFilesGuest() {
-	global $myDB,$myID;
-	echo "<hr><center><br>";
-	if( $row = $myDB->query( "SELECT * FROM  `files` LIMIT 0 , 25" ) ) {
-	    if( $row == 1 ) {
-		    echo "<b>Загруженных файлов не обнаружено</b>";
-			return 0;
-		}
-		echo "<table width='70%' border='1'><tr align='center' bgcolor='#e7e7f0'><td><b>id</b></td><td><b>Имя файла</b></td><td><b>Дата загрузки</b></td><td>&nbsp</td></tr>";
-		$fileUrl = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/upload/';
-		foreach( $row as $file ) {
-			echo "<tr align='center'><td>#".$file['id']."</td><td>".$file['filename']."</td><td>".$file['date']."</td><td><table width='70%'><tr align='center'><td><a href='$fileUrl".$file['filename']."'><img src='images/download.jpg' alt='Скачать'></a></td><td><a href=''><img src='images/comment.jpg' alt='Комментарии'></a></td>";
-			echo "</tr></table></td></tr>";
-		}
-		echo "</table>";
-	}
+    global $myDB,$myID;
+    echo "<hr><center><br>";
+    if( $row = $myDB->query( "SELECT * FROM  `files` LIMIT 0 , 25" ) ) {
+        if( $row == 1 ) {
+            echo "<b>Загруженных файлов не обнаружено</b>";
+            return 0;
+        }
+        echo "<table width='70%' border='1'><tr align='center' bgcolor='#e7e7f0'><td><b>id</b></td><td><b>Имя файла</b></td><td><b>Дата загрузки</b></td><td>&nbsp</td></tr>";
+        $fileUrl = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/upload/';
+        foreach( $row as $file ) {
+            echo "<tr align='center'><td>#".$file['id']."</td><td>".$file['filename']."</td><td>".$file['date']."</td><td><table width='70%'><tr align='center'><td><a href='$fileUrl".$file['filename']."'><img src='images/download.jpg' alt='Скачать'></a></td><td><a href=''><img src='images/comment.jpg' alt='Комментарии'></a></td>";
+            echo "</tr></table></td></tr>";
+        }
+        echo "</table>";
+    }
 }
 
 function showFilesUser() {
-	global $myDB,$myID;
-	if( $row = $myDB->query( "SELECT * FROM  `files` LIMIT 0 , 25" ) ) {
-	    if( $row == 1 ) {
-		    echo "<br><br><hr><center><br><b>Загруженных файлов не обнаружено</b><br>";
-			return 0;
-		}
-		echo "<table width='70%' border='1'><tr align='center' bgcolor='#ffcc00'><td>&nbsp</td><td><b>id</b></td><td><b>Имя файла</b></td><td><b>Дата загрузки</b></td><td>&nbsp</td></tr><form method='post'>";
-		$fileUrl = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/upload/';
-		foreach( $row as $file ) {
-			echo "<tr align='center'><td>&nbsp";
-			if($file['userid'] == $myID )  echo "<input type='checkbox' name='filedel[]' value='".$file['id']."'>";
-			echo "</td><td>#".$file['id']."</td><td>".$file['filename']."</td><td>".$file['date']."</td><td><table width='70%'><tr align='center'><td><a href='$fileUrl".$file['filename']."'><img src='images/download.jpg' alt='Скачать'></a></td><td><a href=''><img src='images/comment.jpg' alt='Комментарии'></a></td>";
-			if($file['userid'] == $myID ) echo "<td><a href='javascript:open(\"".$_SERVER['PHP_SELF']."?option=".$file['id']."\", \"displayWindow\",\"width=400,height=300,status=no,toolbar=no,menubar=no\");'><img src='images/edit.jpg' alt='Опции'></a></td>";
-			echo "</tr></table></td></tr>";
-		}
-		echo "</table><table width='70%'><tr><td><input type='submit' value='удалить отмеченные'></form></td></tr></table>";
-	}
+    global $myDB,$myID;
+    if( $_GET['order'] == 'desc' ) {
+        $order = 'DESC';
+        $_order = 'asc';
+        $image = 's_desc.png';
+    } else {
+        $order = 'ASC';
+        $_order = 'desc';
+        $image = 's_asc.png';
+    }
+    if( $_GET['by'] == 'filename' ) {
+        $by = 'filename';
+    } else {
+        $by = 'date';
+    }
+    if( $row = $myDB->query( "SELECT * FROM  `files` ORDER BY $by $order LIMIT 0 , 25" ) ) {
+        if( $row == 1 ) {
+            echo "<br><br><hr><center><br><b>Загруженных файлов не обнаружено</b><br>";
+            return 0;
+        }
+        echo "<table width='70%' border='1'><tr align='center' bgcolor='#e7e7f0'><td>&nbsp</td><td><b>id</b></td><td><b>Имя файла <a href='?order=$_order&by=filename'><img src='images/$image' border='0'></a></b></td><td><b>Дата загрузки</b> <a href='?order=$_order&by=date'><img src='images/$image' border='0'></a></td><td>&nbsp</td></tr><form method='post' name='formUserFiles'>";
+        $fileUrl = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/upload/';
+        foreach( $row as $file ) {
+            echo "<tr align='center'><td>&nbsp";
+            if($file['userid'] == $myID )  echo "<input type='checkbox' name='filedel[]' value='".$file['id']."'>";
+            echo "</td><td>#".$file['id']."</td><td>".$file['filename']."</td><td>".$file['date']."</td><td><table width='70%'><tr align='center'><td><a href='$fileUrl".$file['filename']."'><img src='images/download.jpg' alt='Скачать'></a></td><td><a href=''><img src='images/comment.jpg' alt='Комментарии'></a></td>";
+            if($file['userid'] == $myID ) echo "<td><a href='javascript:open(\"".$_SERVER['PHP_SELF']."?option=".$file['id']."\", \"displayWindow\",\"width=400,height=300,status=no,toolbar=no,menubar=no\");'><img src='images/edit.jpg' alt='Опции'></a></td>";
+            echo "</tr></table></td></tr>";
+        }
+        echo "</table><table width='70%'><tr><td><div class='bigbut' id='go'><a href='#' onclick='document.formUserFiles.submit();'>удалить отмеченные</a></div></form></td></tr></table>";
+    }
 }
 
 function showUploadForm() {
-    echo "<br><hr><br><form enctype='multipart/form-data' method='post'><input type='file' name='ufile'><br><br><input type='submit' value='Загрузить файл'></form>";
+    echo "<br><hr><br><form enctype='multipart/form-data' method='post' name='uploadForm'><input type='file' name='ufile'><br><br>
+    <div class='bigbut' id='go'><a href='#' onclick='document.uploadForm.submit();'>Загрузить файл</a></div></form>";
 }
 
 function showHeader() {
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 include_once( 'class.sql.php' );
 include_once( 'func.php' );
@@ -10,27 +10,27 @@ if( $_GET['md5'] ) { echo md5(md5(md5($_GET['md5']))); exit; }
 
 if( isset( $_GET['logout'] ) ) {
     setcookie( 'login', '' );
-	header( 'Location: '.$_SERVER['PHP_SELF'] );
+    header( 'Location: '.$_SERVER['PHP_SELF'] );
 }
 
 if( isset( $_GET['register'] ) ) {
     if( $_POST['goreg'] ) {
-	    if( $_POST['pass1'] != $_POST['pass2'] ) echo "<center><font color='red'>пароли не совпадают</font><br><br>";
-		else if( strlen( $_POST['pass1'] ) < 8 ) echo "<center><font color='red'>пароль должен быть не менее 8 символов!</font><br><br>";
-		else {
-		    $myDB = new DB();
-			$sql = "INSERT INTO `onliner`.`users` (`id`, `mail`, `pass`) VALUES (NULL, '".$myDB->secSQL( $_POST['mail'] )."', '".md5(md5(md5($myDB->secSQL($_POST['pass1']))))."');";
-			if( !$myDB->query( $sql ) ) {
-			    echo "<font color='red'>При регистрации возникли проблемы. Обратитесь к администратору.</font>";
-			}
-			$cookie = base64_encode( $myDB->secSQL( $_POST['mail'] ).'###'.md5(md5(md5($myDB->secSQL($_POST['pass1'])))) );
-			setcookie( 'login', $cookie, time()+60*60*24*30 );
-		    header( 'Location: '.$_SERVER['PHP_SELF'] );
-		}
-	}
+        if( $_POST['pass1'] != $_POST['pass2'] ) echo "<div id='incorrectauth'>пароли не совпадают</div>";
+        else if( strlen( $_POST['pass1'] ) < 8 ) echo "<div id='incorrectauth'>пароль должен быть не менее 8 символов!</div>";
+        else {
+            $myDB = new DB();
+            $sql = "INSERT INTO `onliner`.`users` (`id`, `mail`, `pass`) VALUES (NULL, '".$myDB->secSQL( $_POST['mail'] )."', '".md5(md5(md5($myDB->secSQL($_POST['pass1']))))."');";
+            if( !$myDB->query( $sql ) ) {
+                echo "<div id='incorrectauth'>При регистрации возникли проблемы. Обратитесь к администратору.</div>";
+            }
+            $cookie = base64_encode( $myDB->secSQL( $_POST['mail'] ).'###'.md5(md5(md5($myDB->secSQL($_POST['pass1'])))) );
+            setcookie( 'login', $cookie, time()+60*60*24*30 );
+            header( 'Location: '.$_SERVER['PHP_SELF'] );
+        }
+    }
     showHeader();
-	echo "<body><center><form action='".$_SERVER['PHP_SELF']."?register' method='post'><table><tr><td>E-mail:</td><td><input type='text' name='mail'></td></tr><tr><td>Пароль:</td><td><input type='password' name='pass1'></td></tr><tr><td>Повтор:</td><td><input type='password' name='pass2'></td></tr></table><br><div class='bigbut' id='go'><a href='#'>Регистрация</a></div></form>";
-	exit;
+    echo "<body><center><form action='".$_SERVER['PHP_SELF']."?register' method='post' name='regForm'><table><tr><td>E-mail:</td><td><input type='text' name='mail' class='text'></td></tr><tr><td>Пароль:</td><td><input type='password' name='pass1' class='text'></td></tr><tr><td>Повтор:</td><td><input type='password' name='pass2' class='text'></td></tr></table><br><div class='bigbut' id='go'><a href='#' onclick='document.regForm.submit();'>Регистрация</a></div></form>";
+    exit;
 }
 
 $myDB = new DB();
@@ -39,58 +39,56 @@ $myLogin; $myID;
 
 if( !checkAuth( 'check' ) ) {
     if( $_POST['mail'] && $_POST['pass'] ) {
-	    if( checkAuth( 'login' ) ) {
-			//echo "Successfull login";
-			exit;
-		}
-		else echo '<font color="red">incorrect e-mail/pass</font><br><br>';
-	}
+        if( checkAuth( 'login' ) ) {
+            //echo "Successfull login";
+            exit;
+        }
+        else echo '<div id="incorrectauth">incorrect e-mail/pass</div><br><br>';
+    }
     showHeader();
-	echo "<body><center><form method='post'><table><tr><td>E-mail: </td><td><input type='text' name='mail'></td></tr><tr><td>Пароль: </td><td><input type='password' name='pass'></td></tr></table><br><div class='bigbut' id='go'><a href='#'>Войти</a></div> <div class='bigbut' id='go'><a href='".$_SERVER['PHP_SELF']."?register'>Зарегистрироваться</a></div></form> ";
-	
-	showFilesGuest();
-	exit;
+    echo "<body><center><form method='post' name='loginForm'><table><tr><td>E-mail: </td><td><input type='text' name='mail' class='text'></td></tr><tr><td>Пароль: </td><td><input type='password' name='pass' class='text'></td></tr></table><br><div class='bigbut' id='go'><a href='#' onclick='document.loginForm.submit();'>Войти</a></div> <div class='bigbut' id='go'><a href='".$_SERVER['PHP_SELF']."?register'>Зарегистрироваться</a></div></form> ";
+
+    showFilesGuest();
+    exit;
 }
 
 if( $_GET['option'] ) {
     $sql = "SELECT `comment` FROM `files` WHERE `userid`='$myID' AND `id`='".$myDB->secSQL($_GET['option'])."'";
-	if( $row = $myDB->query( $sql ) AND $row != 1 ) {
-	    if( $_POST['save'] ) {
-		    //var_dump( $_POST ); exit;
-			if( $_POST['comment'] == 'on'  ) {
-			    $sql = "UPDATE  `onliner`.`files` SET `comment` = '1' WHERE `files`.`userid`='$myID' AND `files`.`id` ='".$myDB->secSQL($_GET['option'])."';";
-				$c = 1;
-			}
-			else {
-			    $sql = "UPDATE  `onliner`.`files` SET `comment` = '0' WHERE `files`.`userid`='$myID' AND `files`.`id` ='".$myDB->secSQL($_GET['option'])."';";
-				$c = 0;
-			}
-			if( $myDB->query( $sql ) ) {
-			    echo "<b><font color='green'><center>Успешно сохранено</font></b><br><br>";
-				$row[0]['comment'] = $c;
-			}
-			else echo "Не удалось изменить опцию.";
-		}
-		echo "<br><br><center><form action='".$_SERVER['PHP_SELF']."?option=".$myDB->secSQL($_GET['option'])."' method='post'>";
-		if( $row[0]['comment'] == 0 ) {
-		    echo "<input type='checkbox' name='comment'> Разрешение комментировать файл";
-		}
-		else {
-		    echo "<input type='checkbox' name='comment' checked> Разрешение комментировать файл";
-		}
-		echo "<br><br><input type='submit' name='save' value='Сохранить'></form><br><br><a href='javascript:close();'>Закрыть</a>";
-	}
-	else {
-	    echo "Не верный file id.";
-	}
-	exit;
+    if( $row = $myDB->query( $sql ) AND $row != 1 ) {
+        if( $_POST['save'] ) {
+            //var_dump( $_POST ); exit;
+            if( $_POST['comment'] == 'on'  ) {
+                $sql = "UPDATE  `onliner`.`files` SET `comment` = '1' WHERE `files`.`userid`='$myID' AND `files`.`id` ='".$myDB->secSQL($_GET['option'])."';";
+                $c = 1;
+            }
+            else {
+                $sql = "UPDATE  `onliner`.`files` SET `comment` = '0' WHERE `files`.`userid`='$myID' AND `files`.`id` ='".$myDB->secSQL($_GET['option'])."';";
+                $c = 0;
+            }
+            if( $myDB->query( $sql ) ) {
+                echo "<b><font color='green'><center>Успешно сохранено</font></b><br><br>";
+                $row[0]['comment'] = $c;
+            }
+            else echo "Не удалось изменить опцию.";
+        }
+        showHeader();
+        echo "<body><br><br><center><form action='".$_SERVER['PHP_SELF']."?option=".$myDB->secSQL($_GET['option'])."' method='post' name='formOption'><input type='checkbox' name='comment'";
+        if( $row[0]['comment'] != 0 ) {
+            echo "checked";
+        }
+        echo "> Разрешение комментировать файл<br><br><input type='hidden' name='save' value='1'><div class='bigbut' id='go'><a href='#' onclick='document.formOption.submit();'>Сохранить</a></div></form><br><br><a href='javascript:close();'>Закрыть</a>";
+    }
+    else {
+        echo "Не верный file id.";
+    }
+    exit;
 }
 
 if( $_POST['filedel'] ) {
     foreach( $_POST['filedel'] as $delid ) {
-	    $sql = "DELETE FROM `files` WHERE `userid`='$myID' AND `id`='".$myDB->secSQL($delid)."'";
-		$myDB->query( $sql );
-	}
+        $sql = "DELETE FROM `files` WHERE `userid`='$myID' AND `id`='".$myDB->secSQL($delid)."'";
+        $myDB->query( $sql );
+    }
 }
 
 showHeader();
@@ -98,43 +96,43 @@ echo "<body><center><table width='100%'><tr><td width='110%' align='center'>Пр
 
 if( $_FILES['ufile'] ) {
     $ufile      = $_FILES['ufile']['tmp_name'];
-	$ufile_name = $_FILES['ufile']['name'];
-	$ufile_size = $_FILES['ufile']['size'];
-	$ufile_type = $_FILES['ufile']['type'];
-	$ufile_flag = $_FILES['ufile']['error'];
-	
-	if( $ufile_flag == 0 ) {
-	    //echo "File: $ufile<br>File:$ufile_name<br>File type: $ufile_type<br>Size: $ufile_size";
-		if( !copy( $ufile, "upload/$ufile_name" ) ) {
-		    echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" ...<br></font>";
-		}
-		else {
-			$sql = "INSERT INTO `onliner`.`files` (`id`, `filename`, `localname`, `userid`, `date`, `ip`, `ua`) VALUES (NULL, '$ufile_name', '$ufile_name', '$myID', '".date( 'Y-m-d H:i:s' )."', '".$_SERVER['REMOTE_ADDR']."', '".$_SERVER['HTTP_USER_AGENT']."');";
-			
-			if( !$myDB->query( $sql ) ) {
-			    echo "<font color='red'>При добавлении файла возникла ошибка. Обратитесь к администратору.</font>";
-			}
-			echo "<font color='green'>Файл \"".htmlspecialchars($ufile_name)."\" успешно загружен!<br></font><br>";
-		}
-	}
-	else {
-	    switch( $ufile_flag ) {
-			case 1:
-			    echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" [размер файла превышает максимально допустимый #1]<br></font>";
-				break;
-			case 2:
-			    echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" [размер файла превышает максимально допустимый #1]<br></font>";
-				break;
-			case 3:
-			    echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" [часть файла не загружена]<br></font>";
-				break;
-			case 4:
-			    echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" [некорректный путь к файлу]<br></font>";
-				break;
-			default:
-			    echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" ...<br></font>";
-		}
-	}
+    $ufile_name = $_FILES['ufile']['name'];
+    $ufile_size = $_FILES['ufile']['size'];
+    $ufile_type = $_FILES['ufile']['type'];
+    $ufile_flag = $_FILES['ufile']['error'];
+
+    if( $ufile_flag == 0 ) {
+        //echo "File: $ufile<br>File:$ufile_name<br>File type: $ufile_type<br>Size: $ufile_size";
+        if( !copy( $ufile, "upload/$ufile_name" ) ) {
+            echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" ...<br></font>";
+        }
+        else {
+            $sql = "INSERT INTO `onliner`.`files` (`id`, `filename`, `localname`, `userid`, `date`, `ip`, `ua`) VALUES (NULL, '$ufile_name', '$ufile_name', '$myID', '".date( 'Y-m-d H:i:s' )."', '".$_SERVER['REMOTE_ADDR']."', '".$_SERVER['HTTP_USER_AGENT']."');";
+
+            if( !$myDB->query( $sql ) ) {
+                echo "<font color='red'>При добавлении файла возникла ошибка. Обратитесь к администратору.</font>";
+            }
+            echo "<font color='green'>Файл \"".htmlspecialchars($ufile_name)."\" успешно загружен!<br></font><br>";
+        }
+    }
+    else {
+        switch( $ufile_flag ) {
+            case 1:
+                echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" [размер файла превышает максимально допустимый #1]<br></font>";
+                break;
+            case 2:
+                echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" [размер файла превышает максимально допустимый #1]<br></font>";
+                break;
+            case 3:
+                echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" [часть файла не загружена]<br></font>";
+                break;
+            case 4:
+                echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" [некорректный путь к файлу]<br></font>";
+                break;
+            default:
+                echo "<font color='red'>ошибка при загрузке файла \"".htmlspecialchars($ufile_name)."\" ...<br></font>";
+        }
+    }
 }
 showFilesUser();
 showUploadForm();
